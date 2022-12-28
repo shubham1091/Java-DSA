2b387c9d5f59ea3a187bf458b83fd0497762966c
shubham <shubhamverma1091@gmail.com>
2022-12-28 22:25:02 +0530

    Binary tree refactored

--- BinaryTrees/BST/Build.java
+++ BinaryTrees/BST/Build.java
@@ -3,13 +3,24 @@ package BinaryTrees.BST;
 public class Build {
     public static void main(String[] args) {
 
-        int[] values = { 5, 1, 3, 4, 2, 7 };
+        int[] values = { 8, 5, 3, 1, 4, 6, 7, 10, 11, 14 };
         BS tree = new BS();
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = tree.insert(root, values[i]);
         }
         tree.inorder(root);
+
+        System.out.println();
+        root = tree.delete(root, 5);
+        tree.inorder(root);
+
+        System.out.println();
+        if (tree.search(root, 2)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
 
     }
 }
