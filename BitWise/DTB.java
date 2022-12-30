0ea9c44ce4a08529d36364aef620ef119af49740
shubham <shubhamverma1091@gmail.com>
2022-12-30 15:24:42 +0530

    added list of prectice Q in

---
+++ BitWise/DTB.java
@@ -0,0 +1,9 @@
+package BitWise;
+
+public class DTB {
+    //decimal to base b
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- BitWise/Ibit.java
+++ BitWise/Ibit.java
@@ -2,12 +2,25 @@ package BitWise;
 
 public class Ibit {
     public static void main(String[] args) {
-        // int n =7;
-        System.out.println((~0));
+        System.out.println(check(6, 100));
+        System.out.println(set(3, 10));
+        System.out.println(reset(2, 10));
+
     }
 
-    static int check(int n) {
-        return n & (1 << (n - 1));
+    static int check(int n, int x) {
+        return (x & (1 << (n - 1))) == 0 ? 0 : 1;
+    }
+
+    static int set(int n, int x) {
+        int temp = 1 << (n - 1);
+        return x | temp;
+    }
+
+    static int reset(int n, int x) {
+        int temp = 1 << (n - 1);
+        temp = ~temp;
+        return x & temp;
     }
 
 }
